fa01f4ea4697e3b4b5c7ca156e7327709ec05403
Alan Ponte <aponte@Alans-MacBook-Pro.local>
2015-11-15 19:58:11 -0800

    first commit

---
+++ src/main/java/com/upalate/dbjava/DbjavaApplication.java
@@ -0,0 +1,12 @@
+package com.upalate.dbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbjavaApplication.class, args);
+    }
+}
